1da094125232670e2f6c2f46a237408255d0695e
Freddy Fallon <freddyfallon@gmail.com>
2017-06-11 10:24:24 +0100

    Add whitespace stripping and removal of punctuation from strings

--- spec/FormatterSpec.js
+++ spec/FormatterSpec.js
@@ -8,7 +8,53 @@ describe("Formatter", function(){
   describe("format", function(){
     it("should make all words downcase", function() {
       formatter.format("Hello There Guys");
-      expect(formatter.lowerCaseWords).toEqual("hello there guys");
+      expect(formatter.formattedWords).toEqual("hello there guys");
+    });
+
+    it("should remove all punctuation marks", function() {
+      formatter.format("what. is, \\ / your * & name?!");
+      expect(formatter.formattedWords).toEqual("what is your name");
+    });
+
+    it("should remove all whitespace", function() {
+      formatter.format("hello   there");
+      expect(formatter.formattedWords).toEqual("hello there")
+    });
+  });
+
+  describe("makeLowerCase", function(){
+    it("should make words lower case", function() {
+      formatter.makeLowerCase("Hello There Guys");
+      expect(formatter.formattedWords).toEqual("hello there guys");
+    });
+  });
+
+  describe("removePunctuation", function(){
+    it("should remove question marks", function() {
+      formatter.removePunctuation("what is your name?");
+      expect(formatter.formattedWords).toEqual("what is your name");
+    });
+
+    it("should remove all full stops", function() {
+      formatter.removePunctuation("hello...");
+      expect(formatter.formattedWords).toEqual("hello");
+    });
+
+    it("should remove forward and back slashes", function() {
+      formatter.removePunctuation("T/il\\l");
+      expect(formatter.formattedWords).toEqual("Till");
+    });
+
+    it("should remove commas", function() {
+      formatter.removePunctuation("t,h,i,s");
+      expect(formatter.formattedWords).toEqual("this");
+    });
+  });
+
+  describe("stripWhitespace", function() {
+    it("should replace any spaces greater than one with one space", function(){
+      formatter.stripWhitespace("hello     there");
+      expect(formatter.formattedWords).toEqual("hello there");
     });
   });
 });
--- src/Formatter.js
+++ src/Formatter.js
@@ -1,7 +1,21 @@
 function Formatter() {
-  var lowerCaseWords;
+  var formattedWords;
 }
 
 Formatter.prototype.format = function(words) {
-  this.lowerCaseWords = words.toLowerCase();
+  this.removePunctuation(words);
+  this.makeLowerCase(this.formattedWords);
+  this.stripWhitespace(this.formattedWords);
+};
+
+Formatter.prototype.makeLowerCase = function(words) {
+  this.formattedWords = words.toLowerCase();
+};
+
+Formatter.prototype.removePunctuation = function(words) {
+  this.formattedWords = words.replace(/[^\w\s]|_/g, "");
+};
+
+Formatter.prototype.stripWhitespace = function(words) {
+  this.formattedWords = words.replace(/\s+/g, " ");
 };
